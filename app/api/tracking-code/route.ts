ec4e57bb44d74ba85a045e8c661059b255b195dc
HuzaifaSidiqui <huzaifa.siddique5000@gmail.com>
2025-10-21 20:40:27 +0500

    script fixed

--- app/api/tracking-code/route.ts
+++ app/api/tracking-code/route.ts
@@ -37,7 +37,7 @@ export async function GET(request: NextRequest) {
     const script = `<script>
   !function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.async=!0,p.src=s.api_host+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="capture identify alias people.set people.set_once set_config register register_once unregister opt_out_capturing has_opted_out_capturing opt_in_capturing reset isFeatureEnabled onFeatureFlags".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
   
-  console.log('Initializing PostHog with host: ${posthogHost}');
+  // console.log('Initializing PostHog with host: ${posthogHost}');
   
   posthog.init('${posthogKey}', {
     api_host: '${posthogHost}',
@@ -51,25 +51,22 @@ export async function GET(request: NextRequest) {
         email: true,
       },
     },
-    debug: true
+    debug: false
   });
   
-  // Identify user with UXbreak tracking code
   posthog.identify('${trackingCode}');
   
-  // Set user properties
   posthog.people.set({
     'uxbreak_tracking_code': '${trackingCode}',
     'account_type': 'uxbreak_user'
   });
   
-  // Send initialization event
   posthog.capture('uxbreak_tracking_initialized', {
     tracking_code: '${trackingCode}',
     timestamp: new Date().toISOString()
   });
   
-  console.log('UXbreak Analytics fully initialized with PostHog');
+  console.log('UXbreak Analytics fully initialized.');
 </script>`;
 
     return new Response(script, {
